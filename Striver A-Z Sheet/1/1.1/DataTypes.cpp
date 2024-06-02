55a97043b5735c6c4b382501b2cc919be692d88a
Akansh Vaibhav <94189081+akanshv@users.noreply.github.com>
2024-06-02 22:51:02 +0530

    Update DataTypes.cpp

--- Striver A-Z Sheet/1/1.1/DataTypes.cpp
+++ Striver A-Z Sheet/1/1.1/DataTypes.cpp
@@ -1,4 +1,4 @@
-// https://www.naukri.com/code360/problems/data-type_8357232?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+// https://www.naukri.com/code360/problems/data-type_8357232?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf edit
 
 int dataTypes(string type) {
 	if (type =="Integer" || type == "Float"){
